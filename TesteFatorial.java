5ec78c535ecb156e858908782cd104fbf4351905
Carlos Henrique Duarte da Silva <henriqueduartes@gmail.com>
2020-08-22 21:38:33 -0300

    alteração para lancar excecao em caso de numeros negativos

--- TesteFatorial.java
+++ TesteFatorial.java
@@ -1,7 +1,7 @@
 public class TesteFatorial{
 	public static void main(String[] args){
 		Fatorial fat = new Fatorial();
-		int resultado = fat.calculaFatorial(5);
+		int resultado = fat.calculaFatorial(-5);
 		System.out.println(resultado);
 	}
 }
